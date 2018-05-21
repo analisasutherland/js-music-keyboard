d2613724c6b15e7311c0bdec9c4762d42da10749
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-05-21 16:50:54 -0700

    Button note plays and click note plays. will not play or print to console for a nonvalid note

--- noise.js
+++ noise.js
@@ -1,3 +1,20 @@
 $(document).ready( function() {
-  // your code here
+  $('button').click( function(event) {
+    let note = this.innerHTML;
+    console.log(note);
+
+    let audioID = note + "Audio";
+    console.log(audioID);
+
+    document.getElementById(audioID).play();
+  });
+
+  $('button').keydown( function(event) {
+    let key = event.key;
+    let audioID = key + "Audio";
+    if (document.getElementById(audioID) !== null) {
+      console.log(audioID);
+      document.getElementById(audioID).play();
+    }
+  });
 });
